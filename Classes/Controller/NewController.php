dd848ae1c8e96001d59b2e1c190d2065dea73608
Sebastian Iffland <Sebastian.Iffland@gmx.de>
2014-12-03 15:02:22 +0100

    Extended to initializeCreateAction()

--- Classes/Controller/NewController.php
+++ Classes/Controller/NewController.php
@@ -4,6 +4,17 @@ namespace In2\Femanagerextended\Controller;
 class NewController extends \In2\Femanager\Controller\NewController {
 
 	/**
+	 * Init for User creation
+	 *
+	 * @return void
+	 */
+	public function initializeCreateAction() {
+		parent::initializeCreateAction();
+		$propertyMappingConfiguration = $this->arguments['user']->getPropertyMappingConfiguration();
+		$propertyMappingConfiguration->allowProperties('staticCountryZone');
+	}
+
+	/**
 	 * action create
 	 *
 	 * @param \In2\Femanagerextended\Domain\Model\User $user

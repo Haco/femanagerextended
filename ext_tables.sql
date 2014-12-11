2e6642ac7264cfd6e832298217f9d5cf0dedf713
Sebastian Iffland <Sebastian.Iffland@gmx.de>
2014-12-11 10:04:46 +0100

    SQL overwritten field properties

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,6 +2,7 @@
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
+	gender int(11) default '-1',
 	static_country int(11) unsigned DEFAULT '0',
 	static_country_zone int(11) unsigned DEFAULT '0',
 	privacy_policy tinyint(4) unsigned DEFAULT '0' NOT NULL
